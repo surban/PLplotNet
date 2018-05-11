5b69fa726d3b4fe70e981e8bf1393f3dd36fb427
Appveyor CI <appveyor@surban.net>
2018-05-10 18:08:48 -0700

    Appveyor CI deployment

--- Docs/ReleaseNotes.md
+++
@@ -1,5 +0,0 @@
-# Release Notes
-
-##### 5.13.6
-* Improved NuGet package.
-
--- Fetch-Native.ps1
+++
@@ -1,7 +0,0 @@
-$NATIVE_SRC = "https://github.com/surban/PLplot/releases/download/b1/plplot_windows.zip"
-
-
-$ErrorActionPreference = "Stop"
-Invoke-WebRequest $NATIVE_SRC -OutFile windows.zip 
-Expand-Archive windows.zip -DestinationPath "$PSScriptRoot/Native" -Force
-Remove-Item -Force windows.zip
