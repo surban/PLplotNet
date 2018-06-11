e9fdf924dd2406c15a6ed59d7dc496a67974b822
Sebastian Urban <surban@surban.net>
2018-06-11 13:32:46 +0200

    Move F# samples

---
+++ Samples/FSharp/x00/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open PLplot
+
+
+[<EntryPoint>]
+let main argv =
+
+    // create PLplot stream
+    use pl = new PLStream()
+
+    // generate data
+    let nsize = 101
+    let xmin, xmax, ymin, ymax = 0., 1., 0., 100.
+    let x, y =
+        Array.init nsize (fun i ->
+            let x = float i / float (nsize - 1)
+            let y = ymax * x * x
+            x, y)
+        |> Array.unzip
+
+    // Parse and process command line arguments
+    let mutable argv = argv
+    pl.parseopts( &argv, ParseOpts.Full ||| ParseOpts.NoProgram ) |> ignore
+
+    // Initialize plplot
+    pl.init()
+
+    // output version
+    printfn "PLplot version %s" (pl.gver())
+
+    // Create a labelled box to hold the plot.
+    pl.env( xmin, xmax, ymin, ymax, AxesScale.Independent, AxisBox.BoxTicksLabelsAxes )
+    pl.lab( "x", "y=100 x#u2#d", "Simple PLplot demo of a 2D line plot" )
+
+    // Plot the data that was prepared above.
+    pl.line( x, y )
+
+    // PLplot is automatically closed when pl is disposed.
+    0
+    
